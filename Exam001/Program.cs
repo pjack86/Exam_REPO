7eb62b38b4fc336d4bd45ac60aa8ae3baf511f00
gusev <pjack86@mail/com>
2022-06-11 15:09:20 +0500

    Написал код

--- Exam001/Program.cs
+++ Exam001/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string[] array = { "12", "567", "1234", "as", "tyhb" };
+
+string[] result = new string[array.Length];
+int i = 0;
+foreach (string value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[i] = value;
+        i++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, i));
